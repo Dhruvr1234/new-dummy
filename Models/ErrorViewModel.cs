58235d000d761a3df65a86bbf7d08ab7dc497dee
Jay Radadiya <jay.radadiya@etatvasoft.com>
2025-01-10 17:10:39 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace new_dummy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
